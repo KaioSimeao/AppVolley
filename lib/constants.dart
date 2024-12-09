f87cd53ae107f2c522086537116a8f166a36eb68
Kaio Simeão <k.rodrigues1704@gmail.com>
2024-12-09 15:23:41 -0300

    refactor: changing colors and textslyles in constants

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,16 +2,16 @@ import 'package:flutter/material.dart';
 
 class MyColors {
   static const Color kAzulCeu = Color(0xff00ADC3);
-  static const Color kAzulMarinho =Color(0xff2B4A8E);
-  static const Color kLaranjaCenoura =Color(0xffF77859);
+  static const Color kAzulMarinho = Color(0xff2B4A8E);
+  static const Color kLaranjaCenoura = Color(0xffF77859);
   static const Color kGelo = Color(0xffC2F2F8);
   static const Color kAmareloMostarda = Color(0xFFFFA33B);
-  static final Color? kCorFonte = Colors.grey[50];
+  static const Color kCorFonte = Color(0xFFFAFAFA);
 }
 
 class MyTextStyle {
-  static TextStyle text15 = TextStyle(
+  static const TextStyle text15 = TextStyle(
       color: MyColors.kCorFonte, fontSize: 15, fontFamily: 'ConcertOne');
-  static TextStyle text30 = TextStyle(
+  static const TextStyle text30 = TextStyle(
       color: MyColors.kCorFonte, fontSize: 30, fontFamily: 'ConcertOne');
 }
