34ce5fc94390e8b4f1af3d7eae0035697842aa29
Kaio Simeão <k.rodrigues1704@gmail.com>
2024-12-09 15:48:35 -0300

    refactor: changing constant name in constants

--- lib/themes/constants.dart
+++ lib/themes/constants.dart
@@ -6,12 +6,16 @@ class MyColors {
   static const Color kLaranjaCenoura = Color(0xffF77859);
   static const Color kGelo = Color(0xffC2F2F8);
   static const Color kAmareloMostarda = Color(0xFFFFA33B);
-  static const Color kCorFonte = Color(0xFFFAFAFA);
+  static const Color kCorFontePrimaria = Color(0xFFFAFAFA);
 }
 
 class MyTextStyle {
   static const TextStyle text15 = TextStyle(
-      color: MyColors.kCorFonte, fontSize: 15, fontFamily: 'ConcertOne');
+      color: MyColors.kCorFontePrimaria,
+      fontSize: 15,
+      fontFamily: 'ConcertOne');
   static const TextStyle text30 = TextStyle(
-      color: MyColors.kCorFonte, fontSize: 30, fontFamily: 'ConcertOne');
+      color: MyColors.kCorFontePrimaria,
+      fontSize: 30,
+      fontFamily: 'ConcertOne');
 }
