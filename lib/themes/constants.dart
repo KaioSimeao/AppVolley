a4ee497e81f5bd4653f292864498ae3f8e8c2903
Kaio Simeão <k.rodrigues1704@gmail.com>
2024-12-10 11:22:11 -0300

    feat: adding new colors constants

--- lib/themes/constants.dart
+++ lib/themes/constants.dart
@@ -6,7 +6,7 @@ class MyColors {
   static const Color kLaranjaCenoura = Color(0xffF77859);
   static const Color kGelo = Color(0xffC2F2F8);
   static const Color kAmareloMostarda = Color(0xFFFFA33B);
-  static const Color kCorFontePrimaria = Color(0xFFFAFAFA);
+  static const Color kFontePrimaria = Color(0xFFFAFAFA);
 }
 
 class MyTextStyle {
