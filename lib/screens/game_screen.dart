3ff821032280455fbb5ac972f975412112005d9d
Kaio Simeão <k.rodrigues1704@gmail.com>
2024-12-10 05:35:21 -0300

    refactor: removing dead code in game-screen

--- lib/screens/game_screen.dart
+++ lib/screens/game_screen.dart
@@ -184,35 +184,3 @@ class _GameScreenState extends State<GameScreen> {
   }
 }
 
-enum SideOrientation { left, right }
-
-class SideButton extends StatelessWidget {
-  final SideOrientation orientation;
-  final String texto;
-
-  const SideButton({super.key, required this.orientation, required this.texto});
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      leading: orientation == SideOrientation.left
-          ? SizedBox(
-              height: 50,
-              width: 50,
-              child: PlusButton(
-                func: () {},
-              ))
-          : null,
-      title: Text(
-        texto,
-        textAlign: orientation == SideOrientation.left
-            ? TextAlign.left
-            : TextAlign.right,
-        style: MyTextStyle.text30,
-      ),
-      trailing: orientation == SideOrientation.right
-          ? SizedBox(height: 50, width: 50, child: PlusButton(func: () {}))
-          : null,
-    );
-  }
-}
